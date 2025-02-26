c1b1c4dbd31324bd0830ea90020b9400297166ed
Ashish Kushwaha <ashish@wishfy.club>
2025-02-26 23:29:06 +0530

    createReadStream, createWriteStream

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+import fs, { write } from "node:fs";
+import zlib from "node:zlib"; // if you want to zip file without memory usages
+
 import express from "express";
 import status from "express-status-monitor";
-import fs from "node:fs";
-import zlib from "node:zlib"; // if you want to zip file without memory usages
 
 const PORT = 8000;
 
@@ -17,10 +18,39 @@ fs.createReadStream("./sample.txt").pipe(
 );
 
 app.get("/", (req, res) => {
+  // Downloading file wrong way, file can be any binary
+  // const file = fs.readFileSync("./sample.txt");
+  // return res.end(file);
+  //
+  // Downloading file good way
   // on fronted in response header, we can see Transfer-Encoded: chunked
-  const stream = fs.createReadStream("./sample.txt", "utf-8");
-  stream.on("data", (chunk) => res.write(chunk));
-  stream.on("end", () => res.end());
+  // const stream = fs.createReadStream("./sample.txt", "utf-8");
+  // 1.
+  // stream.on("data", (chunk) => res.write(chunk));
+  // stream.on("end", () => res.end());
+  // 2.
+  // stream.pipe(res);
+
+
+  // copy big file using bad way
+  // const file = fs.readFileSync('sample.txt');
+  // fs.writeFileSync('output.txt', file);
+  // res.end();
+  
+  
+  // copy big file using good way
+  const readStream = fs.createReadStream("./sample.txt", "utf-8");
+  const writeStream = fs.createWriteStream('./output.txt')
+
+  readStream.on('data', (chunk) => {
+    // console.log('chunk: buffer', chunk);
+    // console.log('chunk: string', chunk.toString());
+     
+    writeStream.write(chunk);
+  });
+
+  res.end();
+
 });
 
 app.listen(PORT, () => {
--- docs.md
+++ docs.md
@@ -3,3 +3,4 @@
 # https://nodejs.org/docs/latest/api/stream.html
 
 - request and response on http server, net, Zlib, crypto are also streams
+- Request is readable stream, and Response is writable stream
